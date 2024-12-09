6b44f520bbb7123b96c8fcb81d620e552d3fe6e9
AhrooranR <r.ahrooran@gtngroup.com>
2024-12-09 19:40:45 +0530

    Basic App Setup
    1. Basic setup of Spring Boot
    2. Integrated SQL Logger for debugging purpose
    3. Moved application.properties -> application.yaml
    4. Added custom logback.xml
    5. Added custom banner.txt

---
+++ src/main/java/org/ahroo/nexus/configuration/SQLLoggerProxy.java
@@ -0,0 +1,48 @@
+package org.ahroo.nexus.configuration;
+
+import net.ttddyy.dsproxy.ExecutionInfo;
+import net.ttddyy.dsproxy.QueryInfo;
+import net.ttddyy.dsproxy.listener.logging.SLF4JLogLevel;
+import net.ttddyy.dsproxy.listener.logging.SLF4JQueryLoggingListener;
+import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.time.Duration;
+import java.util.List;
+
+@Component
+public class SQLLoggerProxy implements BeanPostProcessor {
+
+    private DataSource encloseWithLoggingProxy(DataSource actual) {
+
+        Duration slowQueryThreshold = Duration.ofSeconds(5);
+
+        SLF4JQueryLoggingListener slowQueryListener = new SLF4JQueryLoggingListener() {
+            @Override
+            public void afterQuery(ExecutionInfo execInfo, List<QueryInfo> queryInfoList) {
+                if (slowQueryThreshold.toMillis() <= execInfo.getElapsedTime()) {
+                    super.afterQuery(execInfo, queryInfoList);
+                }
+            }
+        };
+
+        slowQueryListener.setLogLevel(SLF4JLogLevel.WARN);
+
+        return ProxyDataSourceBuilder
+                .create(actual)
+                .listener(slowQueryListener)
+                .logQueryBySlf4j(SLF4JLogLevel.TRACE)
+                .build();
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof DataSource dataSource) {
+            bean = encloseWithLoggingProxy(dataSource);
+        }
+        return bean;
+    }
+}
---
+++ src/main/java/org/ahroo/nexus/util/Colors.java
@@ -0,0 +1,12 @@
+package org.ahroo.nexus.util;
+
+public class Colors {
+
+    // @formatter:off
+    public static final String ANSI_FG_GREEN    = "\u001B[32m";
+    public static final String ANSI_RESET       = "\u001B[0m";
+    // @formatter:on
+
+    private Colors() {
+    }
+}
